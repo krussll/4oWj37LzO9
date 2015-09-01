2f25f97b325ba9763c781e147da7e1d084c5da6a
krussll <raw987@hotmail.com>
2015-09-01 18:17:01 +0100

    ga update

--- resources/views/shared/master.blade.php
+++ resources/views/shared/master.blade.php
@@ -36,7 +36,16 @@
         <![endif]-->
 <!-- Google fonts -->
       <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Playfair+Display:400italic' rel='stylesheet' type='text/css' />
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+          ga('create', 'UA-67012989-1', 'tagdaq.herokuapp.com');
+          ga('send', 'pageview');
+
+        </script>
 </head>
 
 
@@ -187,16 +196,7 @@
     {!! Minify::javascript('/js/custom.js') !!}
     
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-67012989-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 </body>
 
 </html>
