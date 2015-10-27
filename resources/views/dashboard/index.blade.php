eb1d6b93c7433117bbe82b15b0bf0ea5f2f3de0c
krussll <raw987@hotmail.com>
2015-10-27 07:40:20 +0000

    dashboard changes

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -15,14 +15,31 @@
                   <div ng-show="dashboard.control.isLoading">
                     <img src="/cdn/ajax-loader.gif" />
                   </div>
+                  <table class="table table-striped responsive-utilities">
+                    <thead>
+                    <tr>
+                      <th>Stock</th>
+                      <th class="hidden-xs">Shares</th>
+                      <th class="hidden-xs">Price Taken</th>
+                      <th>Gain/Loss</th>
+                      <th></th>
+                    </tr>
+                    </thead>
+                    <tr ng-repeat="trade in dashboard.activeTrades">
+                      <td>
+                        <a href="/profile/{{trade.profile.id}}">{{trade.profile.name}}</a>
+                        <a href="/profile/{{trade.profile.id}}"><span class="handle">{{trade.profile.handle}}</span></a></td>
+                      <td class="hidden-xs">{{trade.shares_taken}}</td>
+                      <td class="hidden-xs">{{layout.consts.siteCurrency}}{{trade.price_taken | number }}</td>
+                      <td><span ng-class="{'green': (trade.profile.current_price - trade.price_taken ) >= 0, 'red': (trade.profile.current_price - trade.price_taken ) < 0}"><h5>{{layout.consts.siteCurrency}}{{trade.profile.current_price | number }} ({{trade.change | percentageDifference:trade.price_taken:trade.profile.current_price  }})</h5></span></td>
+                      <td><cdn-sell-button button-size="xs" trade-id="{{trade.id}}" event-handler="dashboard.updateTrades()" /></td>
+                    </tr>
+                  </table>
                     <div ng-repeat="trade in dashboard.activeTrades" class="col-md-3 col-xs-12">
                         <div class="tile-stats container">
                           <div class="row name-container">
                             <div class="col-xs-8">
-                              <div class="count">
-                                <a href="/profile/{{trade.profile.id}}">{{trade.profile.name}}</a>
-                                <a href="/profile/{{trade.profile.id}}"><span class="handle">{{trade.profile.handle}}</span></a>
-                              </div>
+
                             </div>
                             <div class="col-xs-4">
                               <div class="right">
